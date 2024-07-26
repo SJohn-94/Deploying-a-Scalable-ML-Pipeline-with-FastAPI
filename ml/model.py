4e83e820a458ec6dfc240b8e4dfffc7792a544b6
Samuel <sjo4550@wgu.edu>
2024-07-26 13:05:25 -0500

    Uploading current version

--- ml/model.py
+++ ml/model.py
@@ -1,6 +1,9 @@
 import pickle
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from ml.data import process_data
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+
 # TODO: add necessary import
 
 # Optional: implement hyperparameter tuning.
@@ -20,7 +23,11 @@ def train_model(X_train, y_train):
         Trained machine learning model.
     """
    # TODO: implement the function
-    pass
+    model = RandomForestClassifier()
+
+    model.fit(X_train, y_train)
+
+    return model
 
 
 def compute_model_metrics(y, preds):
@@ -60,7 +67,9 @@ def inference(model, X):
         Predictions from the model.
     """
     # TODO: implement the function
-    pass
+    preds = model.predict(X)
+
+    return preds
 
 def save_model(model, path):
     """ Serializes model to a file.
@@ -73,12 +82,15 @@ def save_model(model, path):
         Path to save pickle file.
     """
     # TODO: implement the function
-    pass
+    with open(path, 'wb') as f:
+        pickle.dump(model, f)
 
 def load_model(path):
     """ Loads pickle file from `path` and returns it."""
     # TODO: implement the function
-    pass
+    with open(path, 'rb') as f:
+        model = pickle.load(f)
+    return model
 
 
 def performance_on_categorical_slice(
@@ -118,11 +130,29 @@ def performance_on_categorical_slice(
 
     """
     # TODO: implement the function
+    sliced_data = data[data[column_name] == slice_value]
+
+    cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]   
+
     X_slice, y_slice, _, _ = process_data(
-        # your code here
+        sliced_data,
+        categorical_features=cat_features,
+        label="salary",
+        training=False,
+        encoder=encoder,
+        lb=lb
         # for input data, use data in column given as "column_name", with the slice_value 
         # use training = False
     )
-    preds = # your code here to get prediction on X_slice using the inference function
+    preds = inference(model, X_slice)# your code here to get prediction on X_slice using the inference function
     precision, recall, fbeta = compute_model_metrics(y_slice, preds)
     return precision, recall, fbeta
--- model_card_template.md
+++ model_card_template.md
@@ -4,15 +4,47 @@ For additional information see the Model Card paper: https://arxiv.org/pdf/1810.
 
 ## Model Details
 
+For this project, I used RandomForestClassifer to train the model. 
+
 ## Intended Use
+
+The intended use of this project and model is to predict if an individual earns more than 50K per year salary.
 
 ## Training Data
 
+The data used for this project and its details can be found at https://archive.ics.uci.edu/dataset/20/census+income
+
+The data includes the following columns of information on individuals:
+    - age
+    - workclass
+    - education
+    - marital-status
+    - occupation
+    - relationship
+    - race
+    - sex
+    - capital-gain
+    - capital-loss
+    - hours-per-week
+    - native-country
+    - salary
+
 ## Evaluation Data
 
+Data is evaluated by splitting the data into training and testing portions. I split the data with train_test_split and set a test size for 0.2 making the testing set 20% of the data.
+
 ## Metrics
+
+Model Performance:
+
+Precision: 0.7442 | Recall: 0.6334 | F1: 0.6843
+
 _Please include the metrics used and your model's performance on those metrics._
 
 ## Ethical Considerations
 
+The data in this project is public data and does not contain personally identifiable information(PII).
+
 ## Caveats and Recommendations
+
+No recommendations to be made. This project is for learning purposes only. If this were applied in a real-life scenario and PII were present, I would recommend not showing that portion of the data in order to create integrity in the project.
