4585bb1274995a1c60afea734b1d6bf7f3e49a07
Samuel <sjo4550@wgu.edu>
2024-07-28 13:14:06 -0500

    Uploading test_ml.py since tests passed

--- test_ml.py
+++ test_ml.py
@@ -5,6 +5,7 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from ml.model import train_model, inference, compute_model_metrics
+from sklearn.preprocessing import OneHotEncoder, LabelBinarizer
 
 from ml.data import process_data
 from ml.model import (
@@ -16,13 +17,16 @@ from ml.model import (
     train_model,
 )
 
-# TODO: add necessary import
+# TODO: add necessary import - DONE
 
-def data():
-    project_path = "/home/sam_john/Deploying-a-Scalable-ML-Pipeline-with-FastAPI"
-    data_path = os.path.join(project_path, "data", "census.csv")
-    data = pd.read_csv(data_path, sep=',')
-    return data
+# Reading in data so that the tests can be run
+
+project_path = "/home/sam_john/Deploying-a-Scalable-ML-Pipeline-with-FastAPI"
+data_path = os.path.join(project_path, "data", "census.csv")
+print(data_path)
+data = pd.read_csv(data_path, sep=',')
+
+train, test = train_test_split(data, test_size=0.2, random_state=42)
 
 # TODO: implement the first test. Change the function name and input as needed
 def test_model():
@@ -31,10 +35,33 @@ def test_model():
     """
     # Your code here
 
+    # Taking my code from train_model.py to run these tests to ensure correct datatypes are being used and that values for metrics fall within correct bounds
+    
+    cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
+
+    train, test = train_test_split(data, test_size=0.2, random_state=42)
+
+    X_train, y_train, encoder, lb = process_data(
+        train,
+        training=True,
+        label="salary",
+        categorical_features=cat_features
+    )
+
+    model = RandomForestClassifier()
+
     model = train_model(X_train, y_train)
+
     assert isinstance(model, RandomForestClassifier)
-
-
 
 # TODO: implement the second test. Change the function name and input as needed
 def test_data_metrics():
@@ -42,6 +69,39 @@ def test_data_metrics():
     Testing that the metrics fall between 0 and 1 
     """
     # Your code here
+
+    cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
+
+    train, test = train_test_split(data, test_size=0.2, random_state=42)
+
+    X_train, y_train, encoder, lb = process_data(
+        train,
+        training=True,
+        label="salary",
+        categorical_features=cat_features
+    )
+  
+    X_test, y_test, _, _ = process_data(
+        test,
+        categorical_features=cat_features,
+        label="salary",
+        training=False,
+        encoder=encoder,
+        lb=lb,
+    )   
+
+    model = RandomForestClassifier()
+
+    model = train_model(X_train, y_train)
 
     preds = inference(model, X_test)
     precision, recall, fbeta = compute_model_metrics(y_test, preds)
@@ -57,8 +117,42 @@ def test_datatype():
     """
     # Your code here
 
+    cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
+
+    train, test = train_test_split(data, test_size=0.2, random_state=42)
+
+    X_train, y_train, encoder, lb = process_data(
+        train,
+        training=True,
+        label="salary",
+        categorical_features=cat_features
+    )
+  
+    X_test, y_test, _, _ = process_data(
+        test,
+        categorical_features=cat_features,
+        label="salary",
+        training=False,
+        encoder=encoder,
+        lb=lb,
+    )   
+
+    model = RandomForestClassifier()
+
+    model = train_model(X_train, y_train)
+
+    preds = inference(model, X_test)
+
     precision, recall, fbeta = compute_model_metrics(y_test, preds)
     assert isinstance(precision, float)
     assert isinstance(recall, float)
     assert isinstance(fbeta, float)
-
